a569704801d4b0333a8c5d69db7a00d5fc7cbb0c
mahesh <gangurdemahesh9@gmail.com>
2023-03-13 10:41:12 +0530

    [mahesh] add sample program file

---
+++ Sample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello this is Sample Program in RFP 267 batch");
+            TestClass.InformationDisplay();
+        }
+    }
+}
---
+++ Sample/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    public class TestClass
+    {
+        // This method use for Display Information
+        public static void InformationDisplay()
+        {
+            string firstName;
+            int salary;
+            Console.WriteLine("In Test Class");
+        }
+    }
+}
